aa5277a21f9bd28046959037be0d5dca994c4bbb
Radomir Skrzepij <zyziu10@gmail.com>
2024-03-22 22:56:28 +0100

    add astro db dependency with minimalistic config definition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import deno from '@astrojs/deno';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import deno from '@astrojs/deno'
+import sitemap from '@astrojs/sitemap'
 
-import sitemap from '@astrojs/sitemap';
+import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: deno(),
-});
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), db()],
+  output: 'server',
+  adapter: deno()
+})
