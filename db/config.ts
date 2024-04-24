da8d77e2adae67c864941ffccb22d6183dc660c7
Radomir Skrzepij <zyziu10@gmail.com>
2024-04-24 22:09:43 +0200

    add comment db table definition

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,18 @@
-import { defineDb } from 'astro:db';
+import {column, defineDb, defineTable} from 'astro:db';
+
+const Comment = defineTable({
+  columns: {
+    id: column.number({primaryKey: true}),
+    postId: column.number(),
+    author: column.text(),
+    content: column.text(),
+    createdAt: column.date(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Comment
+  }
 });
